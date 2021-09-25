2895d5b62aa7b621070923766b7073390d30dd9c
Kim van Wyk <kimv@sahomeloans.com>
2021-09-25 14:19:13 +0200

    TIL creation script, no CLI yet

---
+++ make_til_entry.py
@@ -0,0 +1,40 @@
+from glob import glob
+import os.path
+import textwrap
+
+import arrow
+
+TIL_DIR = "content/post/til"
+
+
+def get_filename(arrow_date):
+    ds = f"{arrow_date:YYMMDD}"
+    files = glob(os.path.join(TIL_DIR, f"*{ds}*.md"))
+    if files:
+        files.sort()
+        highest = int(files[-1].split(".md")[0].split("_")[-1])
+    else:
+        highest = 0
+    fn = f"til_{ds}_{highest + 1}.md"
+    return fn
+
+
+def make_til_file(arrow_date):
+    fn = get_filename(arrow_date)
+    with open(os.path.join(TIL_DIR, fn), "w") as fh:
+        fh.write(
+            textwrap.dedent(
+                f"""\
+---
+date: "{arrow_date:YYYY-MM-DD}"
+tags: ["til"]
+title: "Today I Learned: {arrow_date:DD/MM/YYYY} - "
+---
+
+"""
+            )
+        )
+
+
+if __name__ == "__main__":
+    make_til_file(arrow.now())
